0d7fe9cd76c7592a78f1a58bfca74033c286bed4
andreagarciapascualbarranco <andreagarciapb@gmail.com>
2021-03-03 18:32:43 +0100

    Update pythonhive3.py

--- pythonhive3.py
+++ pythonhive3.py
@@ -33,7 +33,7 @@ cmd = "hive -S -e 'SELECT * FROM log4jLogs WHERE t4 = \"[ERROR]\";'"
 print ("Ejecutando "+cmd)
 output = subprocess.getstatusoutput(cmd)
 if status == 0:
-	print output
+	print (output)
 else:
-	print "error"
+	print ("error")
 
